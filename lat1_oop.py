efd88a4baf2016fd07d9b604c6bc836d5881fd04
Vidky Adhi Pradana <vickypradana1111@gmail.com>
2024-11-05 12:12:18 +0700

    Menambah comment

--- lat1_oop.py
+++ lat1_oop.py
@@ -1,53 +1,69 @@
 from abc import ABC, abstractmethod
 
+# membuat kelas Makanan
 class Makanan(ABC):
+    
+    # mendeskripsikan fungsi persiapan secara abstrak
     @abstractmethod
     def persiapan():
         pass;
     
+    # mendeskripsikan fungsi waktu_memasak secara abstrak
     @abstractmethod
     def waktu_memasak():
         pass;
     
+    # mendeskripsikan fungsi suhu_memasak secara abstrak
     @abstractmethod
     def suhu_memasak():
         pass;
 
+# membuat kelas Pasta yang diturunkan  dari kelas Makanan
 class Pasta(Makanan):
+    
+    # mengisikan fungsi  persiapan untuk kelas Pasta
     def persiapan(self):
         return ("pasta, air, garam");
     
+    # mengisikan fungsi  waktu_memasak untuk kelas Pasta
     def waktu_memasak(self):
         return ("10 Menit");
-        
+    
+    # mengisikan fungsi suhu_memasak untuk kelas Pasta
     def suhu_memasak(self):
         return ("90 derajat celcius");
 
+# membuat kelas Sup yang diturunkan  dari kelas Makanan
 class Sup(Makanan):
+    
+    # mengisikan fungsi  persiapan untuk kelas Sup
     def persiapan(self):
         return ("air sayur, garam, bumbu");
     
+    # mengisikan fungsi  waktu_memasak untuk kelas Sup
     def waktu_memasak(self):
         return ("20 Menit");
-        
+
+    # mengisikan fungsi  suhu_memasak untuk kelas Sup
     def suhu_memasak(self):
         return ("80 derajat celcius");
-        
+
+
 nama_makanan = input("Nama makanan: ");
 
 if nama_makanan.lower() ==  "pasta":
-    pasta = Pasta();
+    pasta = Pasta(); #membuat objek jika nama_makananya  adalah pasta
     print("mempersiapkan pasta ...");
-    print(f"Bahan\t\t: {pasta.persiapan()}");
-    print(f"Waktu memasak\t: {pasta.waktu_memasak()}");
-    print(f"Suhu\t\t: {pasta.suhu_memasak()}");
+    print(f"Bahan\t\t: {pasta.persiapan()}"); # menampilkan bahan dari pasta
+    print(f"Waktu memasak\t: {pasta.waktu_memasak()}");  # menampilkan waktu memasak dari pasta
+    print(f"Suhu\t\t: {pasta.suhu_memasak()}"); #  menampilkan suhu dari pasta
     
 elif nama_makanan.lower() == "sup":
-    sup = Sup();
-    print("mempersiapkan sup ...");
-    print(f"Bahan\t\t: {sup.persiapan()}");
-    print(f"Waktu memasak\t: {sup.waktu_memasak()}");
-    print(f"Suhu\t\t: {sup.suhu_memasak()}");
+    sup = Sup();  #membuat objek jika nama_makananya  adalah sup
+    print("mempersiapkan sup ...");  # menampilkan persiapan sup
+    print(f"Bahan\t\t: {sup.persiapan()}");  # menampilkan bahan dari sup
+    print(f"Waktu memasak\t: {sup.waktu_memasak()}");   # menampilkan waktu memasak dari sup
+    print(f"Suhu\t\t: {sup.suhu_memasak()}");  # menampilkan suhu dari sup
     
 else:
     print("makanan tidak ditemukan");
--- lat2_oop.py
+++ lat2_oop.py
@@ -1,18 +1,24 @@
 class AkunBank:
+    
+    # konstruktor menambah nama nasabah dan saldo awal
     def __init__(self, nama, saldo):
         self.nama = nama;
-        self.__saldo = saldo;
-        
+        self.__saldo = saldo; # saldo yang bersifat private
+    
+    # method untuk  menambahkan saldo
     def deposit(self, jumlah):
         self.__saldo += jumlah;
     
+    # method untuk penarikan saldo
     def tarik(self, jumlah):
         if jumlah > self.__saldo:
+            # print tidak cukup 
             print("Saldo tidak cukup");
         else:
             self.__saldo -= jumlah;
             print("Penarikan berhasil.");
     
+    # method untuk  menampilkan saldo
     def lihat_saldo(self):
         return self.__saldo;
 
@@ -33,15 +39,19 @@ while True:
         nama = input("Nama nasabah: ");
         saldo = int(input("Saldo awal: "));
         
+        
         if nama in akun_nasabah:
+            # jika nama nasabah sudah ada maka  tampilkan pesan
             print("Nasabah dengan nama ini sudah ada.");
         else:
+            # membuat akun baru jika  nama nasabah belum ada
             akun_nasabah[nama] = AkunBank(nama, saldo)
             print("Akun berhasil dibuat untuk nasabah:", nama);
     
     elif pilihan == 2:
         nama = input("Masukkan nama nasabah: ");
         if nama in akun_nasabah:
+            # jika nasabah ditemukan  maka tampilkan saldonya
             print(f"Saldo  {nama}: {akun_nasabah[nama].lihat_saldo()}");
         else:
             print("Akun nasabah tidak ditemukan.");
@@ -49,7 +59,10 @@ while True:
     elif pilihan == 3:
         nama = input("Masukkan nama nasabah: ");
         if nama in akun_nasabah:
+            # jika nama nasabah ditemukan, masukkan jumlah deposit
             jumlah = int(input("Jumlah deposit: "));
+            
+            # memanggil method deposit() yang melekat pada  objek akun nasabah
             akun_nasabah[nama].deposit(jumlah);
             print("Deposit berhasil.");
         else:
@@ -58,12 +71,16 @@ while True:
     elif pilihan == 4:
         nama = input("Masukkan nama nasabah: ");
         if nama in akun_nasabah:
+            # jika nama nasabah ditemukan, masukkan jumlah penarikan
             jumlah = int(input("Jumlah penarikan: "));
+            
+            # memanggil method tarik() yang melekat pada  objek akun nasabah
             akun_nasabah[nama].tarik(jumlah);
         else:
             print("Akun nasabah tidak ditemukan.");
     
     elif pilihan == 5:
+        # keluar/break
         print("Terima kasih telah menggunakan layanan kami.");
         break;
     
