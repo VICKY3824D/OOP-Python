c99b7e72ed92f943acd4d0fa9bd390003cce0870
Vidky Adhi Pradana <vickypradana1111@gmail.com>
2024-11-05 11:37:20 +0700

    lat2 versi2

--- lat2_oop.py
+++ lat2_oop.py
@@ -14,21 +14,58 @@ class AkunBank:
     
     def lihat_saldo(self):
         return self.__saldo;
-    
-    
 
-print("Pilihan: ");
-print("1. Buat Akun ");
-print("2. Lihat Saldo ");
-print("3. Deposit ");
-print("4. Tarik ");
+# Dictionary untuk menyimpan akun nasabah berdasarkan nama mereka
+akun_nasabah = {};
 
-pilihan =  int(input("Masukkan Pilihan: "));
-if pilihan == 1:
-    nama = input("nama: ");
-    saldo = int(input("saldo awal: "));
-    akun = AkunBank(nama, saldo);
-    print("Akun berhasi dibuat");
+while True:
+    print("\nPilihan: ");
+    print("1. Buat Akun ");
+    print("2. Lihat Saldo ");
+    print("3. Deposit ");
+    print("4. Tarik ");
+    print("5. Keluar ");
+
+    pilihan = int(input("Masukkan Pilihan: "));
     
-elif  pilihan == 2:
-   print()
+    if pilihan == 1:
+        nama = input("Nama nasabah: ");
+        saldo = int(input("Saldo awal: "));
+        
+        if nama in akun_nasabah:
+            print("Nasabah dengan nama ini sudah ada.");
+        else:
+            akun_nasabah[nama] = AkunBank(nama, saldo)
+            print("Akun berhasil dibuat untuk nasabah:", nama);
+    
+    elif pilihan == 2:
+        nama = input("Masukkan nama nasabah: ");
+        if nama in akun_nasabah:
+            print(f"Saldo  {nama}: {akun_nasabah[nama].lihat_saldo()}");
+        else:
+            print("Akun nasabah tidak ditemukan.");
+    
+    elif pilihan == 3:
+        nama = input("Masukkan nama nasabah: ");
+        if nama in akun_nasabah:
+            jumlah = int(input("Jumlah deposit: "));
+            akun_nasabah[nama].deposit(jumlah);
+            print("Deposit berhasil.");
+        else:
+            print("Akun nasabah tidak ditemukan.");
+    
+    elif pilihan == 4:
+        nama = input("Masukkan nama nasabah: ");
+        if nama in akun_nasabah:
+            jumlah = int(input("Jumlah penarikan: "));
+            akun_nasabah[nama].tarik(jumlah);
+            print("Penarikan berhasil.")
+        else:
+            print("Akun nasabah tidak ditemukan.");
+    
+    elif pilihan == 5:
+        print("Terima kasih telah menggunakan layanan kami.");
+        break
+    
+    else:
+        print("Pilihan tidak valid.");
